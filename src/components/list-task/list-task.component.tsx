5f098902f30990f54175216ac838e5b1552af7a5
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2026-07-02 20:17:33 +1200

    support subtasks

--- src/components/list-task/list-task.component.tsx
+++ src/components/list-task/list-task.component.tsx
@@ -14,6 +14,8 @@ import { ProgressBar } from "@/components/progress-bar/progress-bar.component";
 import { useTranslation } from "@/context/language.context";
 import { useQueryClient } from "@tanstack/react-query";
 import { getGetTasksQueryKey, startTask } from "@api";
+import { useButtonMenu } from "@/hook/button-menu.hook";
+import { ContextMenuElement } from "@/context/context-menu.context";
 
 interface Props {
 	task: Task;
@@ -23,6 +25,14 @@ export function ListTask({ task }: Props) {
 	const [isLoading, setIsLoading] = useState(false);
 	const { t } = useTranslation();
 	const queryClient = useQueryClient();
+	const baseLanguageKey = useMemo(
+		() =>
+			task.pluginId
+				? `task.plugin.${task.pluginId}.${task.taskId}`
+				: `task.system.${task.taskId}`,
+		[task.pluginId],
+	);
+
 	const buttonIcon = useMemo<Icon>(() => {
 		if (
 			task.status == TaskStatus.stopped ||
@@ -36,12 +46,29 @@ export function ListTask({ task }: Props) {
 		return IconPlayerStopFilled;
 	}, [task.status, task.resumable]);
 
-	const start = () => {
+	const { onClick } = useButtonMenu(() => {
+		if (!task.subTasks) {
+			return [];
+		}
+
+		return task.subTasks.map((subTaskId) => {
+			const entry: ContextMenuElement = {
+				languageKey: `${baseLanguageKey}.subtask.${subTaskId}.name`,
+				key: subTaskId,
+				onClick: () => start(subTaskId),
+			};
+			return entry;
+		});
+	});
+
+	const start = (subTask: string | null) => {
 		if (isLoading) {
 			return;
 		}
 		setIsLoading(true);
-		startTask(task.uuid)
+		startTask(task.uuid, {
+			subTask,
+		})
 			.then((response) => {
 				queryClient.invalidateQueries({ queryKey: getGetTasksQueryKey() });
 			})
@@ -57,24 +84,31 @@ export function ListTask({ task }: Props) {
 				icon={buttonIcon}
 				style="background"
 				iconSource="tabler"
-				onClick={() => {
+				onClick={(e) => {
 					if (task.status == TaskStatus.running) {
 						console.log("Cancelling task"); // todo: implement
 					} else {
-						start();
+						if (task.subTasks && task.status == TaskStatus.stopped) {
+							onClick(e);
+						} else {
+							start(null);
+						}
 					}
 				}}
 				loading={isLoading}
 			/>
 			<div className={styles.right}>
 				<span className={styles.source}>
-					{task.pluginId ? t(`plugin.${task.pluginId}.name`) : t("task.system")}
+					{t(task.pluginId ? `plugin.${task.pluginId}.name` : "task.system")}
 				</span>
-				<span>
-					{task.pluginId
-						? t(`task.plugin.${task.pluginId}.${task.taskId}.name`)
-						: t(`task.system.${task.taskId}.name`)}
-				</span>
+				<div className={styles.nameContainer}>
+					<span className={styles.name}>{t(`${baseLanguageKey}.name`)}</span>
+					{task.percent !== null && (
+						<span className={styles.percent}>
+							{Math.round(task.percent * 100) / 100}%
+						</span>
+					)}
+				</div>
 				{task.status != TaskStatus.stopped && (
 					<ProgressBar percent={task.percent} loading={task.percent === null} />
 				)}
