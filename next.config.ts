07b7e702e544ddcb9c635a177b3462f63417e123
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2026-08-31 16:01:59 +1200

    use unoptimised images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
 	/* config options here */
 	allowedDevOrigins: ["127.0.0.1"],
 	output: "export",
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
